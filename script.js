6e7630f0b6de026c55328bb7fe84db5756fd5fed
Lavanya <laxmilavanya364@gmail.com>
2023-11-06 13:22:30 +0530

    second commit

--- script.js
+++ script.js
@@ -23,3 +23,19 @@ const slideShow = () => {
     slide.style.transform = `translateX(-${count * 101}%)`;
   });
 };
+
+
+//slider
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
